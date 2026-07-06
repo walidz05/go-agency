11e23b436b598c12f9ceb5c69ffb4b74b347b2ac
walid ziani <walidziani440@example.com>
2026-07-06 13:35:49 +0100

    updte10

--- src/services/contactService.js
+++ src/services/contactService.js
@@ -1,17 +1,22 @@
 const API_URL =
-  "https://script.google.com/macros/s/AKfycbza2PkTuGuaaDuDEXG8qNh-yZVs6fcxpTP8mRSY8yvYDOOKRhnLlfLOBLaCMwb0W25O/exec";
+  "https://script.google.com/macros/s/AKfycbxfyi994TJkvZiexU6rzv66ge_17P-gmIAtTJ9sB7gwOAZ7IIKTrPtNCLHAXW76p0Y/exec";
 
 export const sendContact = async (formData) => {
   const body = new FormData();
 
-  Object.keys(formData).forEach((key) => {
-    body.append(key, formData[key]);
+  Object.entries(formData).forEach(([key, value]) => {
+    body.append(key, value);
   });
 
   const response = await fetch(API_URL, {
     method: "POST",
     body,
+    redirect: "follow",
   });
 
-  return response.json();
+  const text = await response.text();
+
+  console.log(text);
+
+  return text;
 };
