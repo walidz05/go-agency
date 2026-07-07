2f9b9f4427f83f33c27d743c0d9dc40ac0c22b4f
walid ziani <walidziani440@example.com>
2026-07-07 14:14:20 +0100

    sinner

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from "react-router-dom";
-import { lazy, Suspense } from "react";
-import { Loader } from "lucide-react";
+import { lazy } from "react";
+
 const Home = lazy(() => import("../pages/Home/Home"));
 const Apropos = lazy(() => import("../pages/apropos/Apropos"));
 const Services = lazy(() => import("../pages/services/Services"));
@@ -10,14 +10,12 @@ const Contact = lazy(() => import("../pages/contact/Contact"));
 function Router() {
   return (
     <>
-      <Suspense fallback={<Loader />}>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/apropos" element={<Apropos />} />
-          <Route path="/services" element={<Services />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-      </Suspense>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/apropos" element={<Apropos />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </>
   );
 }
